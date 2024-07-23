1849a05d10edff428f211e24ab8e527b7a71e922
Fatma Birel <fatmabrl11@gmail.com>
2024-07-23 14:21:44 +0300

    Add project files.

---
+++ Business/Concrete/PersonManager.cs
@@ -0,0 +1,42 @@
+﻿using Business.Abstract;
+using Entitites.Concrete;
+using MernisServiceReference;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+
+    public class PersonManager : IApplicantService
+    {
+        //Encapsulation
+        public void ApplyForMask(Person person)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool CheckPerson(Person person)
+        {
+            KPSPublicSoapClient client = new KPSPublicSoapClient(KPSPublicSoapClient.EndpointConfiguration.KPSPublicSoap);
+
+            var response = client.TCKimlikNoDogrulaAsync(
+                person.NationalIdentity,
+                person.FirstName,
+                person.LastName,
+                person.DateOfBirthYear
+            ).Result;
+
+            return response.Body.TCKimlikNoDogrulaResult;
+        }
+
+
+
+        public List<Person> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Workaround/Program.cs
@@ -0,0 +1,22 @@
+﻿using Business.Concrete;
+using Entitites.Concrete;
+using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            person.NationalIdentity = 56842447180;
+            person.FirstName = "Fatma";
+            person.LastName = "Birel";
+            person.DateOfBirthYear = 2001;
+
+            PttManager pttManager = new PttManager(new PersonManager());
+            pttManager.GiveMask(person);
+            Console.ReadLine();
+        }
+    }
+}
